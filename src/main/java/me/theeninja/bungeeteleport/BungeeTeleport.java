5e16e42f30aa7d5951233e377a0a451147615cc8
TheeNinja <theeninja.development@gmail.com>
2016-07-23 20:32:12 -0700

    Off for the night.

--- src/main/java/me/theeninja/bungeeteleport/BungeeTeleport.java
+++ src/main/java/me/theeninja/bungeeteleport/BungeeTeleport.java
@@ -3,12 +3,20 @@ package me.theeninja.bungeeteleport;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BungeeTeleport extends JavaPlugin {
+	BungeeTeleport plugin;
 	@Override
 	public void onEnable() {
-		
+		plugin = this;
+		registerListeners();
 	}
 	@Override
 	public void onDisable() {
-		
+		plugin = null;
+	}
+	public BungeeTeleport getInstance() {
+		return plugin;
+	}
+	public void registerListeners() {
+		getServer().getPluginManager().registerEvents(new SignBuildListener(), this);
 	}
 }
--- src/main/java/me/theeninja/bungeeteleport/SignBuildListener.java
+++ src/main/java/me/theeninja/bungeeteleport/SignBuildListener.java
@@ -1,11 +1,25 @@
 package me.theeninja.bungeeteleport;
 
+import org.bukkit.ChatColor;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 
-public class SignBuildListener {
+public class SignBuildListener implements Listener {
 	@EventHandler
-	public void onSignBuild(BlockPlaceEvent event) {
-		
+	public void onSignBuild(SignChangeEvent e) {
+		Sign s = (Sign) e.getBlock().getState();
+		e.getPlayer().sendMessage("waddup");
+		if (s.getLine(0).equalsIgnoreCase("[Bungee]")) {
+			e.getPlayer().sendMessage("3");
+			return;
+		}
+		Player p = e.getPlayer();
+		if (!p.isOp()) {
+			p.sendMessage("No permission.");
+		}
+		s.setLine(0, ChatColor.DARK_PURPLE + "[BungeeTeleport");
 	}
 }

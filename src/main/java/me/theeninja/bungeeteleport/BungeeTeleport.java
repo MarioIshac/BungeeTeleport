607fab571625d334afcaec8aea28336d5932b33a
TheeNinja <theeninja.development@gmail.com>
2016-07-23 17:21:36 -0700

    Added main class.

---
+++ src/main/java/me/theeninja/bungeeteleport/BungeeTeleport.java
@@ -0,0 +1,14 @@
+package me.theeninja.bungeeteleport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeTeleport extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+}

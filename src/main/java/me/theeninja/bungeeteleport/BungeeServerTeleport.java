5a9f4095b2e6ccb580ee6118df3b8fca38c64093
TheeNinja <theeninja.development@gmail.com>
2016-09-17 15:44:45 -0700

    Updated BungeeServerTeleport handling.

--- src/main/java/me/theeninja/bungeeteleport/BungeeServerTeleport.java
+++ src/main/java/me/theeninja/bungeeteleport/BungeeServerTeleport.java
@@ -1,5 +1,10 @@
 package me.theeninja.bungeeteleport;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
@@ -8,31 +13,57 @@ import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 
 public class BungeeServerTeleport implements PluginMessageListener {
-	private String[] serverList;
+	private List<String> serverList = new ArrayList<String>();
+	private String server;
 	BungeeServerTeleport(Player player, String server) {
-		updateServers();
-		ByteArrayDataOutput out = ByteStreams.newDataOutput();
-		out.writeUTF("Connect");
-		out.writeUTF(server);
-		player.sendMessage("Connecting to server " + server + "!");
-		player.sendPluginMessage(BungeeTeleport.getInstance(), "BungeeCord", out.toByteArray());
+		this.server = server;
+		player.sendMessage("BungeeServerTeleport instance created.");
+		player.sendMessage("The server has been updated to " + this.server);
+		updateServerList(player);
 	}
 	public BungeeServerTeleport() {
 	
 	}
 	@Override
 	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		player.sendMessage("The plugin message has been received.");
 		if (!channel.equals("BungeeCord")) {
 			return;
 		}
+		player.sendMessage("The plugin message is of type BungeeCord.");
 		ByteArrayDataInput in = ByteStreams.newDataInput(message);
 		String subchannel = in.readUTF();
 		if (subchannel.equals("GetServers")) {
-			serverList = in.readUTF().split(", ");
+			BungeeTeleport.getInstance().getServer().broadcastMessage("The server list has been updated.");
+			serverList = Arrays.asList(in.readUTF().split(", "));
+			player.sendMessage(serverList.toString());
+			player.sendMessage("Calling connect, player, server string is currently: " + server);
+			connectPlayer(player);
 		}
 	}
-	protected static void updateServers() {
+	private void connectPlayer(Player player) {
+		if (player == null) {
+			Bukkit.broadcastMessage("Player is null");
+		}
+		player.sendMessage("Starting to connect player.");
+		ByteArrayDataOutput out = ByteStreams.newDataOutput();
+		player.sendMessage(serverList.toString());
+		player.sendMessage("Trying to connect to: >" + server + "<");
+		if (serverList.contains(server)) {
+			out.writeUTF("Connect");
+			out.writeUTF(server);
+			player.sendMessage("Connecting to server " + server + "!");
+			player.sendPluginMessage(BungeeTeleport.getInstance(), "BungeeCord", out.toByteArray());
+		}
+		else {
+			player.sendMessage("Invalid server " + server);
+		}
+	}
+	private void updateServerList(Player player) {
+		BungeeTeleport.getInstance().getServer().broadcastMessage("updateServerList called");
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF("GetServers");
+		BungeeTeleport.getInstance().getServer().broadcastMessage("serverList trying to be updated.");
+		player.sendPluginMessage(BungeeTeleport.getInstance(), "BungeeCord", out.toByteArray());
 	}
 }
--- src/main/java/me/theeninja/bungeeteleport/SignClickListener.java
+++ src/main/java/me/theeninja/bungeeteleport/SignClickListener.java
@@ -8,10 +8,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class SignClickListener implements Listener {
 	@EventHandler
 	public void onSignClick(PlayerInteractEvent e) {
+		if (!(e.getHand().equals(EquipmentSlot.HAND))) return;
+		e.getPlayer().sendMessage("Event has been called.");
 		if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
 			return;
 		}
@@ -21,12 +24,14 @@ public class SignClickListener implements Listener {
 		}
 		Sign sign = (Sign) block.getState();
 		if (!signCheck(sign)) {
+			e.getPlayer().sendMessage("Sign Check has failed.");
 			return;
 		}
-		e.getPlayer().sendMessage("Listener passed.");
+		e.getPlayer().sendMessage("Event successfully processed.");
+		e.getPlayer().sendMessage("Server pre-instance creation is: " + sign.getLine(1));
 		new BungeeServerTeleport(e.getPlayer(), sign.getLine(1));
 	}
 	private boolean signCheck(Sign sign) {
-		return ChatColor.stripColor(sign.getLine(0)).equals("[BungeeCordTeleport");
+		return ChatColor.stripColor(sign.getLine(0)).equals("[BungeeTeleport]");
 	}
 }

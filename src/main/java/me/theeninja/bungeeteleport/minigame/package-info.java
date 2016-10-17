1a4d492933e57d60d473363a9c41d5021d1fd4e8
TheeNinja <theeninja.development@gmail.com>
2016-10-16 21:26:13 -0700

    Pre-Implementation of option player information option to be displayed on signs.

--- src/main/java/me/theeninja/bungeeteleport/minigame/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by TheeNinja on 10/12/2016.
- */
-package me.theeninja.bungeeteleport.minigame;
--- src/main/java/me/theeninja/bungeeteleport/package-info.java
+++ src/main/java/me/theeninja/bungeeteleport/package-info.java
@@ -1,4 +1,1 @@
-/**
- * Created by TheeNinja on 10/12/2016.
- */
 package me.theeninja.bungeeteleport;
